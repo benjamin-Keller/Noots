1c2187c4c3ec791a578302cdc3c3af2b1558d606
Benjamin Keller <bk.benjaminkeller@gmail.com>
2022-12-28 01:04:50 +0200

    Adds note deleting

--- src/App.tsx
+++ src/App.tsx
@@ -67,6 +67,12 @@ function App() {
     })
   }
 
+  function onDeleteNote(id: string) {
+    setNotes(prevNotes => {
+      return prevNotes.filter(note => note.id !== id)
+    })
+  }
+
   function addTag(tag: Tag) {
     setTags(prev => [...prev, tag])
   }
@@ -80,7 +86,7 @@ function App() {
                                       onAddTag={addTag}
                                       availableTags={tags} />}/>
         <Route path="/:id" element={<NoteLayout notes={notesWithTags} />}>
-          <Route index element={<Note />} />
+          <Route index element={<Note onDelete={onDeleteNote} />} />
           <Route path="edit" element={<EditNote onSubmit={onUpdateNote}
                                       onAddTag={addTag}
                                       availableTags={tags} />} />
--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -1,10 +1,15 @@
 import { Badge, Button, Col, Row, Stack } from "react-bootstrap";
 import ReactMarkdown from "react-markdown";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useNote } from "./NoteLayout";
 
-export function Note() {
+type NoteProps = {
+    onDelete: (id: string) => void
+}
+
+export function Note({ onDelete }: NoteProps) {
     const note = useNote()
+    const navigate = useNavigate() 
 
     return (
         <>
@@ -24,7 +29,10 @@ export function Note() {
                         <Link to={`/${note.id}/edit`}>
                             <Button variant="primary">Edit</Button>
                         </Link>
-                        <Button variant="outline-danger">Delete</Button>
+                        <Button variant="outline-danger" onClick={() => {
+                            onDelete(note.id)
+                            navigate("/")
+                        }}>Delete</Button>
                         <Link to="..">
                             <Button variant="outline-secondary">Back</Button>
                         </Link>
