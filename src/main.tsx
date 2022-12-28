196d8047a18a2d74013974ed7a97b3ab1c9f4ca7
Benjamin Keller <bk.benjaminkeller@gmail.com>
2022-12-28 21:52:23 +0200

    PWA adjustments

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
-import { registerSW } from 'virtual:pwa-register';
-
-if ("serviceWorker" in navigator) {
-  // && !/localhost/.test(window.location)) {
-  registerSW();
-}
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
