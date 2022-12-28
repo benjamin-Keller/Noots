bddedc4c9d9030d5306a0c32d4e86c65d80a3cf8
Benjamin Keller <bk.benjaminkeller@gmail.com>
2022-12-28 02:30:30 +0200

    basename test

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/'>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
